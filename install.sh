3cc462be4d64231d295c2937a80540b0bea19eca
Chanda Mulenga <dev@stctheproducer.com>
2023-08-18 10:10:16 +0000

    feat: add shebang

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 # Install spaceship prompt
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt" --depth=1
 
@@ -17,3 +19,5 @@ ln -nfs "$HOME/dotfiles/spaceship.zsh" "$HOME/.config/spaceship.zsh"
 # ln -nfs "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
 ln -nfs "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
 ln -nfs "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
+
+source $HOME/.zshrc
